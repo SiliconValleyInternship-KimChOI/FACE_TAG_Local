a6ece5130e3b4bfb27c43402c80098bcfed1ac70
김서영 <ksy990628@gmail.com>
2021-07-07 15:31:35 +0900

    result ui 0.0.1

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './js/Main'
+import Result from './js/Result'
 function App() {
   return (
     <div>
-      <Main></Main>
+      {/* <Main></Main> */}
+      <Result></Result>
     </div>
   );
 }
--- front/src/js/Result.js
+++ front/src/js/Result.js
@@ -1,1 +1,46 @@
 import react from 'react'
+import styled from "styled-components";
+import '../css/index.css';
+import Logo from '../css/GAGA2.png';
+import ReactPlayer from 'react-player'
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+//   background-color: #e1f1e7;
+  background-size: cover;
+`;
+
+const Box = styled.div`
+  width:100%;
+  height:50%;
+  border: 0px;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+`
+const Text = styled.p`
+  color: black;
+  font-size:2em;
+  margin-top:15%;
+  font-family: 'Do Hyeon';
+`
+
+const Result= () => {
+    return(
+        <Container>
+            <img src={Logo} style={{width:"13%", height:"26%", position:"absolute", margin: "2% 44%"}}></img>
+            <Box>
+                <Text>동영상 인물 태깅 완료! </Text>
+            </Box>
+            <Box>
+            <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+            </Box> 
+        </Container>
+    )
+}
+
+export default Result;
