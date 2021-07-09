7e39c5f43386ee3663fb4d6af93f467c1cf1774f
ohhondgi <5ghdrl@naver.com>
2021-07-09 13:50:10 +0900

    docs: Add ignored file

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import Main from './js/Main'
 import Result from './js/Result'
-import { BrowserRouter,Route, Link,Switch } from "react-router-dom";
+import {Route} from "react-router-dom";
 
 function App() {
   return (
