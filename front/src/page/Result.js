51268e932567d9be70a73080ab3ec4a2fe25352d
김서영 <ksy990628@gmail.com>
2021-07-15 17:18:17 +0900

    timeline ui

--- front/src/page/Result.js
+++ front/src/page/Result.js
@@ -1,10 +1,14 @@
 import React , {useState} from 'react';
-import styled from "styled-components";
+import styled from 'styled-components';
 import '../css/index.css';
 import axios from 'axios';
 import Logo from '../css/GAGA2.png';
-import {Link} from "react-router-dom";
+import {Link} from 'react-router-dom';
 import ReactPlayer from 'react-player'
+
+
+//임시로 사진 사용
+import harry from "../css/harry.jpg"
 
 const Container = styled.div`
   position: absolute;
@@ -35,6 +39,23 @@ const Img = styled.img`
   position: absolute;
   margin: 1% 45%;
 `
+const Table = styled.table`
+  margin: 0% 15%;
+  width:70%;
+  border: 1px solid;
+  border-radius: 15px;
+  align-items:center;
+  text-align:center;
+  font-family: 'Do Hyeon';
+`
+const Tr = styled.tr`
+  border: 1px solid;
+`
+
+const Td = styled.td`
+  border-top: 1px solid;
+  font-size: 1.4em;
+`
 
 const Result= (props) => {
   const filename = props.location.aboutProps
@@ -58,10 +79,37 @@ const Result= (props) => {
             <Box>
                 <Text>동영상 인물 태깅 완료! </Text>
             </Box>
+            <button onClick = {onClick}>click</button>
+            
+            <ReactPlayer url={uploadedurl} height='200px' controls={controlState}/>
             <Box>
-              <button onClick = {onClick}>click</button>
-            <ReactPlayer url={uploadedurl} height='300px' controls={controlState}/>
-            </Box> 
+              <Text>등장인물 타임라인</Text>
+              </Box>
+            <Table>
+              <Tr>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Timeline</th>
+              </Tr>
+
+              <Tr>
+              <Td><img src={harry} height="200px"/></Td>
+              <Td>Harry Potter</Td>
+              <Td>00:30 00:59 01:22</Td>
+              </Tr>
+
+              <Tr>
+              <Td><img src={harry} height="200px" /></Td>
+              <Td>Ron Weasley</Td>
+              <Td>00:11 00:50</Td>
+              </Tr>
+
+              <Tr>
+              <Td><img src={harry} height="200px" /></Td>
+              <Td>Hermione Granger</Td>
+              <Td>00:30 00:59 01:22</Td>
+              </Tr>
+              </Table>
         </Container>
     )
 }
