c996e2edbe2309d497eee5c1cff01c898999825f
Dongheon97 <dongheon.lee97@gmail.com>
2021-07-21 18:26:01 +0900

    change directory name & add appear_list.txt code

--- backend/tasks.py
+++ backend/tasks.py
@@ -5,13 +5,22 @@
 from celery import Celery
 
 # detect.py
-import yolov5.detect as detect
+from yolov5.detect_test import main, run
 
 
 BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 app = Celery('tasks', broker=BROKER_URL, backend=CELERY_RESULT_BACKEND)
 
+
 @app.task
 def add(x, y):
   return x + y
+
+@app.task
+def get_db(path):
+    #detect = main(path)
+    db_return = run(path)
+    print(db_return)
+
+
