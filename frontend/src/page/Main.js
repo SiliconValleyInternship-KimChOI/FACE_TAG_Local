fb7b442d63ea975989bcd0ca6669b3d819fb7ef7
seungmi lee <71055964+minewet@users.noreply.github.com>
2021-07-26 19:56:49 +0900

    ui 수정

--- frontend/src/page/Main.js
+++ frontend/src/page/Main.js
@@ -1,70 +1,137 @@
-import react from 'react';  
-import styled from "styled-components";
-import '../css/index.css';
-import Logo from '../css/GAGA2.png';
-import {Link} from "react-router-dom";
-import VideoUpload from "../component/VideoUpload";
+import React, {useState} from 'react';
+// 모듈 설치
+import {Link} from 'react-router-dom';   //react link
+import ReactPlayer from 'react-player';  //react video tag
+import styled from 'styled-components';  //css
+import {Form} from 'antd';               //form
+import Dropzone from 'react-dropzone';   //file upload
+import axios from 'axios';               //post
+import Loading from './Loading';
 
-// #수직정렬
-// display:flex;
-// align-items:center;
-// justify-content:center;
-
-const Container = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-size: cover;
-`;
-const Header = styled.div`
-  width:100%;
-  height:20%;
-  border: 0px;
-  display:flex;
+const VideoBox = styled.div`
+border: 3px solid #DCDCDC;
+background: white;
+padding:2%;
+float: left;
+@media only screen and (min-width: 1300px) {
+    width:850px;
+    height:500px;
+    margin:50px 15px 40px 0;
+}
+@media only screen and (max-width: 1300px) {   
+    width: 95%;
+    margin:30px 0 0 0;
+}
 `
-const Box = styled.div`
-  width:100%;
-  height:40%;
-  border: 0px;
-  display:flex;
-  align-items:center;
-  justify-content:center;
+const BtnBox = styled.div`
+border: 3px solid #DCDCDC;
+background: white;
+padding:2%;
+float: left;
+width:400px;
+@media only screen and (min-width: 1300px) {
+    height:500px;
+    margin:50px 0 40px 15px;
+}
+@media only screen and (max-width: 1300px) {
+    margin:30px 0 40px 0;
+}
 `
-const Text = styled.p`
-  color: black;
-  font-size:2em;
-  margin-top:15%;
-  font-family: 'Do Hyeon';
-`
-const Name = styled.p`
-  font-family :'Do Hyeon';
-  font-size: 2em;
-  text-align:center;
-  margin-top: 10%;
-  margin-left: 45%;
-`
-const Img = styled.img`
-  width: 13%;
-  height: 26%;
-  position: absolute;
-  margin: 5% 28%;
+const Div = styled.div`
+display:flex;    
+justify-content: center;
+align-items: center;
+@media only screen and (max-width: 1300px) {
+    flex-direction: column;
+    width:100%;
+}
 `
 
-const Main = () => {
-    return(
-        <Container>
+const SelectBtn = styled.button`
+    width: 300px;
+    height: 60px;
+    color: white;
+    font-size: 1em;
+    margin: 0 0 15px 0;
+    background-color: black;
+    border: 3px solid #DCDCDC;
+    border-radius: 15px;
+    text-align: center;
+    text-decoration-line: none;
+    font-family: 'NanumSquare', sans-serif;
+    &:hover{
+        background: var(--button-hover-bg-color, #404040);
+    }
+`
+const UploadBtn = styled.button`
+    width: 300px;
+    height: 60px;
+    color: #C3C3C3;
+    font-size: 1em;
+    margin: 0 0 15px 0;
+    background-color: white;
+    border: 3px solid #DCDCDC;
+    border-radius: 15px;
+    text-align: center;
+    font-family: 'NanumSquare', sans-serif;
+    &:hover{
+        background: var(--button-hover-bg-color, #404040);
+    }
+`
 
-          <Header>
-          <Link to = '/'><Img src={Logo}/></Link>
-          <Name>GAGAGAGA Video Tag</Name>
-          </Header>
 
-          <Box><Text>동영상을 업로드하고, 해당 영상의 등장 인물을 구분해보세요!</Text></Box>
-          <Box><VideoUpload></VideoUpload></Box>
-        </Container>
-    )
+const VideoUpload = () => {
+    const [uploadedurl, setUploadedurl] = useState(null);  //video url
+    const [controlState, setControlState] = useState(false); //video control
+    const [name,setName] = useState(null); //video file name
+    const [loading,setLoading] = useState(false);
+    const [llink,setLlink] = useState('/'); //routing link
+    const onDrop = (files) => {  
+        setLoading(true);
+        let formData = new FormData()
+        const config = {
+            header: { 'content-type': 'multipart/form-data'}
+        }
+        formData.append('file', files[0])
+        axios.post('http://localhost:5000/fileUpload', formData, config)
+        .then((response) => {
+            setLoading(false);
+            setUploadedurl(URL.createObjectURL(files[0]));
+            setControlState(true);
+            console.log(response.data);
+            const tempname = JSON.stringify(response.data,['name'],0);
+            const tempname2 = JSON.parse(tempname).name;
+            setName(tempname2);
+            setLlink('/result');
+        }).catch(err=>{setLoading(true);})
+    }
+
+    return (
+        <Div>
+            <VideoBox>
+                <div className="title">Video</div>
+                <ReactPlayer url={uploadedurl} controls={controlState}></ReactPlayer>
+                {loading ? <Loading/> : <p></p>}
+            </VideoBox>
+            <BtnBox>
+                <div className="title">Upload</div>
+                <div className="button">
+                <Form onSubmit>
+                    <Dropzone
+                        accept='video/*'
+                        onDrop={onDrop}
+                        multiple={false}    // 한번에 파일을 2개 이상 올릴건지
+                        maxSize={100000000}    // 최대 사이즈 
+                        >
+                    {({getRootProps, getInputProps}) => (
+                    <SelectBtn {...getRootProps()}>파일 선택<input {...getInputProps()} /></SelectBtn >)}
+                    </Dropzone>
+                <Link to = {{pathname: llink, aboutProps: {name}}}><UploadBtn>인물 태깅</UploadBtn></Link>
+                </Form> 
+                </div>
+            </BtnBox>
+        </Div>
+    );
 }
 
-export default Main;
+export default VideoUpload;
