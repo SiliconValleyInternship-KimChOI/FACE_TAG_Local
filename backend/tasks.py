83a92dc089a606b27d765357f3c9fbc6d3541c5e
Dongheon.Lee <32729389+Dongheon97@users.noreply.github.com>
2021-07-27 23:53:25 +0900

    Update tasks.py

--- backend/tasks.py
+++ backend/tasks.py
@@ -3,15 +3,15 @@
 # Python 3.8.8
 # Windows : celery -A tasks worker --pool=solo -l info
 # Mac : celery -A tasks worker --loglevel=info 
-#from celery import Celery
+from celery import Celery
 from yolov5.detect import Detect_class
 
 # Message Broker => Redis
 BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-#celery = Celery('tasks', broker=BROKER_URL, backend=CELERY_RESULT_BACKEND)
+celery = Celery('tasks', broker=BROKER_URL, backend=CELERY_RESULT_BACKEND)
 
-#@celery.task
+@celery.task
 def processing(path):
   detect = Detect_class(path)
   timeline = detect.run()
